25d4d74386309ab0c03aebaecdbe206497bf7068
Marek Vician <marc.vician@gmail.com>
2020-12-04 21:07:39 +0100

    Added implementations of sample_pagerank and iterate_pagernak funcktions

--- 2_uncertainty/pagerank/pagerank.py
+++ 2_uncertainty/pagerank/pagerank.py
@@ -21,6 +21,17 @@ def main():
         print(f"  {page}: {ranks[page]:.4f}")
 
 
+def test():
+    corpus = crawl('corpus2')
+    ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
+    sum = 0
+    print(f'PageRank Results from Sampling (n = {SAMPLES})')
+    for page in sorted(ranks):
+        print(f' {page}: {ranks[page]:.4f}')
+        sum += ranks[page]
+    print(f'Sum of all probabilities: {sum}')
+
+
 def crawl(directory):
     """
     Parse a directory of HTML pages and check for links to other pages.
@@ -57,15 +68,16 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    pages_pdist = {}
-    total_count = len(corpus)
-    linked_count = len(corpus[page])
+    pages_pdist = {}  # Probability distribution
+    total_count = len(corpus)  # Number of all pages in the corpus
+    linked_count = len(corpus[page])  # Number of pages linked to by the page
 
     for p in corpus:
+        pages_pdist[p] = (1 - damping_factor) / total_count
         if p in corpus[page]:
-            pages_pdist[p] = damping_factor / linked_count
-        else:
-            pages_pdist[p] = (1 - damping_factor) / unlinked_count
+            pages_pdist[p] += damping_factor / linked_count
+
+    return pages_pdist
 
 
 def sample_pagerank(corpus, damping_factor, n):
@@ -77,7 +89,35 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    pageranks = {page: 0 for page in corpus.keys()}
+    # First sample is chosen at random
+    sample = random.choice(list(corpus.keys()))
+    idx = n
+
+    while idx > 0:
+        pageranks[sample] += 1  # Update sample's pagerank
+
+        # Chose next sample according to transition model
+        trmodel = transition_model(corpus, sample, DAMPING)
+        population = list(trmodel.keys())
+        weights = [trmodel[page] for page in population]
+        sample = random.choices(population, weights)[0]
+
+        idx -= 1
+
+    return {page: pageranks[page] / n for page in corpus}
+
+
+def pageranks_max_delta(iter_a, iter_b):
+    """Retrun the maximum difference between the ranks of two iterations."""
+    deltas = []
+
+    for page in iter_a:
+        rank_a = iter_a[page]
+        rank_b = iter_b[page]
+        deltas.append(abs(rank_a - rank_b))
+
+    return max(deltas)
 
 
 def iterate_pagerank(corpus, damping_factor):
@@ -89,8 +129,29 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    # Initilize pageranks dictionary with the starting values of 1 / N
+    pageranks = {page: 1 / len(corpus) for page in corpus}
+    # Calculate the constant part of the formula for iterated pageranks
+    prconstant = (1 - damping_factor) / len(corpus)
+
+    while True:
+        new_pageranks = {}
+
+        for page in pageranks:
+            backlinks = [p for p in corpus if page in corpus[p]]
+            psum = 0
+
+            for backlink in backlinks:
+                psum += pageranks[backlink] / len(corpus[backlink])
+
+            new_pageranks[page] = prconstant + damping_factor * psum
+
+        if pageranks_max_delta(pageranks, new_pageranks) <= 0.001:
+            return new_pageranks
+        else:
+            pageranks = new_pageranks
 
 
 if __name__ == "__main__":
     main()
+    # test()
